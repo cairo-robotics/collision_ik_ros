33dce312bef044d249e0effd9775b3cae72c113b
Carl <cmuell89@gmail.com>
2022-07-26 16:45:46 -0600

    UYsing the solver as a ros node and just pinging the results during IK

--- src/collision_ik_service.py
+++ src/collision_ik_service.py
@@ -22,33 +22,6 @@ from visualization_msgs.msg import InteractiveMarkerFeedback, InteractiveMarkerU
 from cairo_planning_core import CONFIG_PATH, Agent
 
 
-def marker_feedback(msg, agent):
-    pos_arr = (ctypes.c_double * 3)()
-    quat_arr = (ctypes.c_double * 4)()
-    pos_arr[0] = msg.pose.position.x
-    pos_arr[1] = msg.pose.position.y
-    pos_arr[2] = msg.pose.position.z
-    quat_arr[0] = msg.pose.orientation.x
-    quat_arr[1] = msg.pose.orientation.y
-    quat_arr[2] = msg.pose.orientation.z
-    quat_arr[3] = msg.pose.orientation.w
-    # Call the rust callback function
-    agent.dynamic_obstacle_cb(msg.marker_name, pos_arr, quat_arr)
-
-def marker_update(msg, agent):
-    # update dynamic collision obstacles in relaxed IK
-    for pose_stamped in msg.poses:
-        pos_arr = (ctypes.c_double * 3)()
-        quat_arr = (ctypes.c_double * 4)()
-        pos_arr[0] = pose_stamped.pose.position.x
-        pos_arr[1] = pose_stamped.pose.position.y
-        pos_arr[2] = pose_stamped.pose.position.z
-        quat_arr[0] = pose_stamped.pose.orientation.x
-        quat_arr[1] = pose_stamped.pose.orientation.y
-        quat_arr[2] = pose_stamped.pose.orientation.z
-        quat_arr[3] = pose_stamped.pose.orientation.w
-        # Call the rust callback function
-        agent.dynamic_obstacle_cb(pose_stamped.name, pos_arr, quat_arr)
 class CollisionIKServiceHandler():
     
     def __init__(self, env_settings_file_path):
@@ -72,47 +45,19 @@ class CollisionIKServiceHandler():
         self.rusty_agent = Agent(env_settings_file_path, False, False)
         
         # Publishers
-        time_pub = rospy.Publisher('/collision_ik/current_time', Float64, queue_size=10)
         self.ee_pose_pub = rospy.Publisher('/collision_ik/ee_pose_goals', EEPoseGoals, queue_size=10)
-        self.jas_pub = rospy.Publisher('/collision_ik/joint_angle_solutions', JointAngles, queue_size=10)
 
-        marker_feedback_cb = partial(marker_feedback, agent=self.rusty_agent)
-        marker_update_cb = partial(marker_update, agent=self.rusty_agent)
-            
-        # Subscribers use to update the collision object positions etc in the Rust-based CollisionIK agent.
-        self.marker_feedback_sub = rospy.Subscriber('/simple_marker/feedback', InteractiveMarkerFeedback, marker_feedback_cb)
-        self.marker_update_sub = rospy.Subscriber('/simple_marker/update', InteractiveMarkerUpdate, marker_update_cb)
-    
+        # the solver loops in a separate process in real time and published to this topic. We pass the results along as the current IK result.
+        self.jas_pub = rospy.Subscriber('/collision_ik/joint_angle_solutions', JointAngles, self._current_ja_cb)
+        ja = JointAngles()
+        ja.angles.data = [0, 0, 0, 0, 0, 0, 0]
+        self.current_joint_angles = ja
+
     def _handle_ik_call(self, req):
-        pose_goals = req.ee_pose_goals.ee_poses
+        self.ee_pose_pub.publish(req.ee_pose_goals)
         header = req.ee_pose_goals.header
         self.ee_pose_pub.publish(req.ee_pose_goals)
-        pos_arr = []
-        quat_arr = []
-        for i in range(len(pose_goals)):
-            p = pose_goals[i]
-            pos_arr.append(p.position.x)
-            pos_arr.append(p.position.y)
-            pos_arr.append(p.position.z)
-
-            quat_arr.append(p.orientation.x)
-            quat_arr.append(p.orientation.y)
-            quat_arr.append(p.orientation.z)
-            quat_arr.append(p.orientation.w)
-
-        xopt = self.rusty_agent.collision_ik(pos_arr, quat_arr)
-        ja = JointAngles()
-        ja.header = header
-        ja_str = "["
-        for i in range(xopt.length):
-            ja.angles.data.append(xopt.data[i])
-            ja_str += str(xopt.data[i])
-            if i == xopt.length - 1:
-                ja_str += "]"
-            else: 
-                ja_str += ", "
-        self.jas_pub.publish(ja)
-        return ja
+        return self.current_joint_angles
 
     def _handle_fk_call(self, req):
         fk_results = self.rusty_agent.forward_kinematics([value for value in req.angles.data])
@@ -130,6 +75,9 @@ class CollisionIKServiceHandler():
         pose.orientation.w = init_rot[3]
 
         return pose
+
+    def _current_ja_cb(self, msg):
+        self.current_joint_angles = msg
         
 
 def main(args=None):
